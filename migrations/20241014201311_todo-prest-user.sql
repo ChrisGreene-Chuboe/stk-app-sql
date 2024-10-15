832580d4f964ae457ec513bfe01d5be51aae9eb3
Chuck Boecking <chuck@chuboe.com>
2024-10-14 20:12:14 -0500

    removed inherit when granting auth role

--- migrations/20241014201311_todo-prest-user.sql
+++ migrations/20241014201311_todo-prest-user.sql
@@ -3,5 +3,5 @@
 create role postgrest_web_anon nologin;
 grant usage on schema public to postgrest_web_anon;
 grant all on todo to postgrest_web_anon;
-create role postgrest login ;
+create role postgrest noinherit login ;
 grant postgrest_web_anon to postgrest;
