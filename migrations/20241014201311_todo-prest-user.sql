3aa27d2d5a97fe9c85085d2b2c03e8fe57902558
Chuck Boecking <chuck@chuboe.com>
2024-10-15 20:23:51 -0500

    added api. to todo grant

--- migrations/20241014201311_todo-prest-user.sql
+++ migrations/20241014201311_todo-prest-user.sql
@@ -2,6 +2,6 @@
 --create schema api
 create role postgrest_web_anon nologin;
 grant usage on schema api to postgrest_web_anon;
-grant all on stk_todo to postgrest_web_anon;
+grant all on api.stk_todo to postgrest_web_anon;
 create role postgrest noinherit login ;
 grant postgrest_web_anon to postgrest;
