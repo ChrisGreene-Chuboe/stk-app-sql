1c11cf36de6f73b6bfff752f45d74d35ff0b74fc
Chuck Boecking <chuck@chuboe.com>
2024-10-15 19:54:45 -0500

    postgresql-convention timestamptz - removed public schema and added private and api

--- migrations/20241014201311_todo-prest-user.sql
+++ migrations/20241014201311_todo-prest-user.sql
@@ -1,7 +1,7 @@
 -- Add migration script here
 --create schema api
 create role postgrest_web_anon nologin;
-grant usage on schema public to postgrest_web_anon;
-grant all on todo to postgrest_web_anon;
+grant usage on schema api to postgrest_web_anon;
+grant all on stk_todo to postgrest_web_anon;
 create role postgrest noinherit login ;
 grant postgrest_web_anon to postgrest;
