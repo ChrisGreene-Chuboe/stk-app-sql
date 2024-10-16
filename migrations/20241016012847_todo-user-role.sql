503d80d03c516620863803afa7d34daa4557d756
Chuck Boecking <chuck@chuboe.com>
2024-10-15 20:34:59 -0500

    added todo_user role

---
+++ migrations/20241016012847_todo-user-role.sql
@@ -0,0 +1,12 @@
+-- Add migration script here
+
+-- create user role
+CREATE ROLE stk_todo_user NOLOGIN;
+GRANT CONNECT ON DATABASE stk_todo_db TO stk_todo_user;
+GRANT USAGE ON SCHEMA api TO stk_todo_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA api TO stk_todo_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA api TO stk_todo_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA api GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO stk_todo_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA api GRANT ALL ON SEQUENCES TO stk_todo_user;
+
+ALTER ROLE stk_todo_user SET search_path TO api;
