d1437e9f6fd697737a585e048023c97d8e497690
Chuck Boecking <chuck@chuboe.com>
2024-10-25 12:24:17 -0500

    updated to remove some create statements

--- migrations/20241009214146_todo-seed-table.sql
+++ migrations/20241009214146_todo-seed-table.sql
@@ -1,10 +1,10 @@
 
-CREATE ROLE stk_todo_superuser NOLOGIN;
+--CREATE ROLE stk_todo_superuser NOLOGIN;
 
-ALTER DATABASE stk_todo_db OWNER TO stk_todo_superuser;
+--ALTER DATABASE stk_todo_db OWNER TO stk_todo_superuser;
 
 -- Create and configure private schema
-CREATE SCHEMA IF NOT EXISTS private AUTHORIZATION stk_todo_superuser;
+CREATE SCHEMA IF NOT EXISTS private;
 GRANT USAGE, CREATE ON SCHEMA private TO stk_todo_superuser;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA private TO stk_todo_superuser;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA private TO stk_todo_superuser;
@@ -12,7 +12,7 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA private GRANT SELECT, INSERT, UPDATE, DELETE 
 ALTER DEFAULT PRIVILEGES IN SCHEMA private GRANT ALL ON SEQUENCES TO stk_todo_superuser;
 
 -- Create and configure api schema
-CREATE SCHEMA IF NOT EXISTS api AUTHORIZATION stk_todo_superuser;
+CREATE SCHEMA IF NOT EXISTS api;
 GRANT USAGE, CREATE ON SCHEMA api TO stk_todo_superuser;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA api TO stk_todo_superuser;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA api TO stk_todo_superuser;
@@ -22,7 +22,7 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA api GRANT ALL ON SEQUENCES TO stk_todo_superu
 ALTER ROLE stk_todo_superuser SET search_path TO private, api;
 
 --Note: leaving public schema since used by sqlx migration
-ALTER SCHEMA public OWNER TO stk_todo_superuser;
+--ALTER SCHEMA public OWNER TO stk_todo_superuser;
 
 --SET ROLE stk_todo_superuser;
 
@@ -31,7 +31,7 @@ CREATE TABLE private.stk_todo (
   created TIMESTAMPTZ NOT NULL DEFAULT now(),
   updated TIMESTAMPTZ NOT NULL DEFAULT now(),
   is_active BOOLEAN NOT NULL DEFAULT true,
-  date_completed TIMESTAMPTZ NOT NULL DEFAULT now(),
+  date_completed TIMESTAMPTZ,
   name TEXT NOT NULL,
   description TEXT,
   date_due TIMESTAMPTZ
