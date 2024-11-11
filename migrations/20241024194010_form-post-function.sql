6e820f0171d08f4443dff339936034b93d5c647a
Chuck Boecking <chuck@chuboe.com>
2024-11-11 10:52:04 -0600

    changed form to jsonb to allow for easier and better manipulation of json data in db

--- migrations/20241024194010_form-post-function.sql
+++ migrations/20241024194010_form-post-function.sql
@@ -6,12 +6,12 @@ CREATE TABLE IF NOT EXISTS private.stk_form_post (
   created TIMESTAMPTZ NOT NULL DEFAULT now(),
   updated TIMESTAMPTZ NOT NULL DEFAULT now(),
   is_active BOOLEAN NOT NULL DEFAULT true,
-  form_data json NOT NULL,
+  form_data jsonb NOT NULL,
   description TEXT
 );
 COMMENT ON TABLE private.stk_form_post IS 'table used to receive html form posts';
 
-CREATE OR REPLACE FUNCTION api.stk_form_post_fn(json) RETURNS "text/html" AS $$
+CREATE OR REPLACE FUNCTION api.stk_form_post_fn(jsonb) RETURNS "text/html" AS $$
 BEGIN
     INSERT INTO private.stk_form_post (form_data)
     VALUES ($1);
@@ -19,6 +19,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql
 SECURITY DEFINER;
-COMMENT ON FUNCTION api.stk_form_post_fn(json) IS 'api function used to write to stk_form_post table';
+COMMENT ON FUNCTION api.stk_form_post_fn(jsonb) IS 'api function used to write to stk_form_post table';
 
-GRANT EXECUTE ON FUNCTION api.stk_form_post_fn(json) TO postgrest_web_anon;
+GRANT EXECUTE ON FUNCTION api.stk_form_post_fn(jsonb) TO postgrest_web_anon;
